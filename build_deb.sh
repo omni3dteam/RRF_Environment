0099d7a98bf3de5277b2da3286c2aadea12de809
Adam <awa@omni3d.net>
2025-04-09 08:03:26 +0100

    Add files

---
+++ build_deb.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+echo $SCRIPT_DIR
+
+PACKAGE_NAME="omnirrf_3.6.0-1.0_arm64"
+echo $PACKAGE_NAME
+
+mkdir -p $PACKAGE_NAME/opt/dsf/sd/firmware_omni
+cp bin/* $PACKAGE_NAME/opt/dsf/sd/firmware_omni
+
+cd $PACKAGE_NAME
+DIR_NAME=$(basename $PWD)
+echo $DIR_NAME
+cd ..
+
+dpkg-deb --build --root-owner-group $DIR_NAME 
+
+cd $SCRIPT_DIR
